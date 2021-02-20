5b0adf40e4b5f57772bf98424c7e4630ac28aed4
lennert-mdware <lennert@mdware.org>
2021-02-20 13:19:45 +0100

    update

--- askimet.js
+++
@@ -1,4 +0,0 @@
-import { AkismetClient } from 'akismet-api';
-
-const { ASKIMET_KEY } = process.env;
-export const askiClient = new AkismetClient({ key: ASKIMET_KEY, blog: 'https://haegepoorters.be' });
